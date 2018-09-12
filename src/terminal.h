361a7ec704f6a2ec690d5b06191d4faa3bb26e85
Ian Henderson <ian@ianhenderson.org>
2018-09-11 17:49:08 -0700

    Add some #defines that MSYS2 needs.

--- src/terminal.h
+++ src/terminal.h
@@ -8,4 +8,12 @@ int terminal_colors(int fileno);
 
 extern bool force_terminal_colors;
 
+// These are needed for MSYS2 (msys2-x86_64-20180531) on Windows.
+#ifndef STDOUT_FILENO
+#define STDOUT_FILENO 1
+#endif
+#ifndef STDERR_FILENO
+#define STDERR_FILENO 2
+#endif
+
 #endif
