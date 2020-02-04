94eb6402c44cb2592500f20eb71d1a24eadcc724
Ian Henderson <ian@ianhenderson.org>
2020-02-04 07:09:28 -0800

    Add support for custom prefixes to the code generator.

--- src/6a-generate.h
+++ src/6a-generate.h
@@ -11,6 +11,10 @@ struct generator {
     struct combined_grammar *combined;
     struct deterministic_grammar *deterministic;
     struct grammar_version version;
+
+    // If non-null, all symbols beginning with owl_ or parsed_ will begin with
+    // prefix_ instead.
+    const char *prefix;
 };
 
 void generate(struct generator *);
